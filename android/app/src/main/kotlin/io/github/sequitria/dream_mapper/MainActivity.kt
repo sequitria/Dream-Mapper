6f33088793f4063b0d08341ecab5529e47b8abc6
StevenT04 <steventhomas1504@gmail.com>
2025-02-13 19:14:13 +0000

    Initital Commint

---
+++ android/app/src/main/kotlin/io/github/sequitria/dream_mapper/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.github.sequitria.dream_mapper
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DreamMapper());
+}
+
+class DreamMapper extends StatelessWidget {
+  const DreamMapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+
+      ),
+    );
+  }
+}
